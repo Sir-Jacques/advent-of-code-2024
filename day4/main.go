f4c5da14525642532c0d9929913dc9a26e1316ce
jacco <jaccomook.98@gmail.com>
2024-12-04 11:48:09 +0100

    solved day4

--- day4/main.go
+++ day4/main.go
@@ -10,9 +10,7 @@ import (
 
 type CrossWord struct {
 	horizontals []string
-	verticals   []string
-	diagonalsLR []string
-	diagonalsRL []string
+	grid        [][]uint8
 }
 
 func main() {
@@ -21,113 +19,77 @@ func main() {
 	input := readInput(filepath.Join(filepath.Dir(filename), "input.txt"))
 
 	crossWord := CrossWord{horizontals: input}
-	crossWord.generateVerticals()
-	crossWord.generateDiagonalsLR()
-	crossWord.generateDiagonalsRL()
-
-	fmt.Println("debug")
-
-	// Parse input into crossword
+	crossWord.generateGrid()
 
 	// Part 1
 	xmasCount := 0
-	for _, word := range crossWord.horizontals {
-		xmasCount += countSubstring(word, "XMAS")
-		xmasCount += countSubstring(word, "SAMX")
+	for i := 0; i < len(crossWord.grid); i++ {
+		for j := 0; j < len(crossWord.grid[i]); j++ {
+			xmasCount += crossWord.matchSubstringAllDirs(i, j, "XMAS")
+		}
 	}
-	for _, word := range crossWord.verticals {
-		xmasCount += countSubstring(word, "XMAS")
-		xmasCount += countSubstring(word, "SAMX")
-	}
-	for _, word := range crossWord.diagonalsRL {
-		xmasCount += countSubstring(word, "XMAS")
-		xmasCount += countSubstring(word, "SAMX")
-	}
-	for _, word := range crossWord.diagonalsLR {
-		xmasCount += countSubstring(word, "XMAS")
-		xmasCount += countSubstring(word, "SAMX")
-	}
+
 	// Part 2
+	crossMasCount := 0
+	for y := 0; y < len(crossWord.grid); y++ {
+		for x := 0; x < len(crossWord.grid[y]); x++ {
+			if crossWord.posIsCrossMas(x, y) {
+				crossMasCount++
+			}
+		}
+	}
 
 	fmt.Println(xmasCount)
+	fmt.Println(crossMasCount)
 }
 
-func countSubstring(input string, substring string) int {
-	fmt.Printf("string: %s\n matches: %d\n\n", input, strings.Count(input, substring))
-	return strings.Count(input, substring)
+func (cw *CrossWord) matchSubstringAllDirs(xPos int, yPos int, word string) int {
+	count := 0
+	for xDiff := -1; xDiff <= 1; xDiff++ {
+		for yDiff := -1; yDiff <= 1; yDiff++ {
+			if xDiff == 0 && yDiff == 0 {
+				continue
+			}
+			if cw.matchSubstringsForPosition(xPos, yPos, xDiff, yDiff, word) {
+				count++
+			}
+		}
+	}
+	return count
 }
 
-func reverseString(s string) string {
-	// Convert string to a slice of runes
-	runes := []rune(s)
-
-	// Reverse the slice of runes
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
+// Current char must the be A, looking for crossing MAS in diagonal directions
+func (cw *CrossWord) posIsCrossMas(xPos int, yPos int) bool {
+	if xPos < 1 || yPos < 1 || xPos >= len(cw.grid[0])-1 || yPos >= len(cw.grid)-1 {
+		return false // Search area out of bounds
+	}
+	if cw.grid[yPos][xPos] != 'A' {
+		return false // Not an A
 	}
 
-	// Convert the slice of runes back to a string
-	return string(runes)
+	// Return whether 2 diagonal "MAS" are found, also count backwards hits
+	return (cw.grid[yPos+1][xPos-1] == 'M' && cw.grid[yPos-1][xPos+1] == 'S' || cw.grid[yPos+1][xPos-1] == 'S' && cw.grid[yPos-1][xPos+1] == 'M') &&
+		(cw.grid[yPos-1][xPos-1] == 'M' && cw.grid[yPos+1][xPos+1] == 'S' || cw.grid[yPos-1][xPos-1] == 'S' && cw.grid[yPos+1][xPos+1] == 'M')
 }
 
-func (cw *CrossWord) generateVerticals() {
-	for i := range cw.horizontals {
-		var vertical string
-		for j := range cw.horizontals {
-			vertical += string(cw.horizontals[j][i])
+func (cw *CrossWord) matchSubstringsForPosition(xPos int, yPos int, xDiff int, yDiff int, word string) bool {
+	for i := 0; i < len(word); i++ {
+		x := xPos + i*xDiff
+		y := yPos + i*yDiff
+		if x < 0 || x >= len(cw.grid) || y < 0 || y >= len(cw.grid[0]) {
+			return false // Search area out of bounds
 		}
-		cw.verticals = append(cw.verticals, vertical)
+		if cw.grid[y][x] != word[i] {
+			return false // Mismatch at current character
+		}
 	}
+	return true
 }
 
-func (cw *CrossWord) generateDiagonalsLR() {
-	x := 0
-	y := 0
-
-	// Bottom left
-	for x < len(cw.horizontals) {
-		var diagonal string
-		for i := x; i < len(cw.horizontals); i++ {
-			diagonal += string(cw.horizontals[i][y])
-			y++
-		}
-		cw.diagonalsLR = append(cw.diagonalsLR, diagonal)
-		x++
-		y = 0
-	}
-
-	// Top right
-	x = 0
-	y = 1 // Exclude duplicate entry of first diagonal
-	for y < len(cw.horizontals[0]) {
-		var diagonal string
-		for i := y; i < len(cw.horizontals[0]); i++ {
-			diagonal += string(cw.horizontals[x][i])
-			x++
-		}
-		cw.diagonalsLR = append(cw.diagonalsLR, diagonal)
-		x = 0
-		y++
-	}
-}
-
-func (cw *CrossWord) generateDiagonalsRL() {
-	// Top left
-	for x := len(cw.horizontals) - 1; x >= 0; x-- {
-		var diagonal string
-		for i := x; i >= 0; i-- {
-			diagonal += string(cw.horizontals[i][x-i])
-		}
-		cw.diagonalsRL = append(cw.diagonalsRL, diagonal)
-	}
-
-	// Bottom right
-	for y := 1; y < len(cw.horizontals[0]); y++ {
-		var diagonal string
-		for i := y; i < len(cw.horizontals[0]); i++ {
-			diagonal += string(cw.horizontals[len(cw.horizontals[0])-1-i][i])
-		}
-		cw.diagonalsRL = append(cw.diagonalsRL, diagonal)
+func (cw *CrossWord) generateGrid() {
+	cw.grid = make([][]uint8, len(cw.horizontals))
+	for i, word := range cw.horizontals {
+		cw.grid[i] = []uint8(word)
 	}
 }
 
