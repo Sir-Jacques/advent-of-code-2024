12b092841316a8354816edc7dffa8a8898f39926
jacco <jaccomook.98@gmail.com>
2024-12-05 18:26:41 +0100

    Simplify code

--- day5/main.go
+++ day5/main.go
@@ -8,15 +8,13 @@ import (
 	aoc "github.com/sir-jacques/advent-of-code-2024/helpers"
 )
 
-// A rule describes which page must come before the other
 type Rule struct {
 	before int
 	after  int
 }
 
 type Book struct {
-	pages        []int
-	indexedPages map[int]int
+	pages []int
 }
 
 func main() {
@@ -28,7 +26,6 @@ func main() {
 	var books []Book
 	inputSector := 0
 	for _, line := range input {
-		// Next sector
 		if line == "" {
 			inputSector++
 			continue
@@ -36,24 +33,19 @@ func main() {
 
 		switch inputSector {
 		case 0:
-			// Parse rule
+			// Parse Rule
 			vals := strings.Split(line, "|")
 			beforePage, _ := strconv.Atoi(vals[0])
 			afterPage, _ := strconv.Atoi(vals[1])
 			rules = append(rules, Rule{before: beforePage, after: afterPage})
 		case 1:
 			// Parse Book
-			var pages []int
 			stringPages := strings.Split(line, ",")
-			for _, p := range stringPages {
-				intPage, _ := strconv.Atoi(p)
-				pages = append(pages, intPage)
+			pages := make([]int, len(stringPages))
+			for i, p := range stringPages {
+				pages[i], _ = strconv.Atoi(p)
 			}
-			book := Book{pages: pages}
-			book.indexPages()
-			books = append(books, book)
-		default:
-			fmt.Println("Invalid sector")
+			books = append(books, Book{pages: pages})
 		}
 	}
 
@@ -64,6 +56,7 @@ func main() {
 			sumOfCorrectMiddlePages += book.getMiddlePage()
 		}
 	}
+	fmt.Println(sumOfCorrectMiddlePages)
 
 	// Part 2
 	sumOfFixedMiddlePages := 0
@@ -73,60 +66,37 @@ func main() {
 			sumOfFixedMiddlePages += book.getMiddlePage()
 		}
 	}
-
-	fmt.Println(sumOfCorrectMiddlePages)
 	fmt.Println(sumOfFixedMiddlePages)
 }
 
 func (b *Book) fixPages(rules []Rule) {
 	for i := 0; i < len(b.pages); i++ {
 		for j := i + 1; j < len(b.pages); j++ {
-			// Check if pages violate any rule
-			followsRules := true
+			// Check if pages violate any rule, flip them if so
 			for _, rule := range rules {
 				if b.pages[i] == rule.after && b.pages[j] == rule.before {
-					followsRules = false
+					b.pages[i], b.pages[j] = b.pages[j], b.pages[i]
 				}
-			}
-
-			// Flip wrong pages around
-			if !followsRules {
-				buffer := b.pages[i]
-				b.pages[i] = b.pages[j]
-				b.pages[j] = buffer
 			}
 		}
 	}
-	b.indexPages()
 }
 
-func (b *Book) indexPages() {
-	b.indexedPages = make(map[int]int)
-	for i, page := range b.pages {
-		b.indexedPages[page] = i
-	}
-}
-
-// Part 1
 func (b *Book) getMiddlePage() int {
-	middleIndex := len(b.pages) / 2
-	return b.pages[middleIndex]
+	return b.pages[len(b.pages)/2]
 }
 
 func (b *Book) followsAllRules(rules []Rule) bool {
+	indexedPages := make(map[int]int)
+	for i, page := range b.pages {
+		indexedPages[page] = i
+	}
 	for _, rule := range rules {
-		if !b.followsRule(rule) {
+		beforeIndex, beforeExists := indexedPages[rule.before]
+		afterIndex, afterExists := indexedPages[rule.after]
+		if beforeExists && afterExists && beforeIndex > afterIndex {
 			return false
 		}
 	}
 	return true
-}
-
-func (b *Book) followsRule(rule Rule) bool {
-	beforeIndex, beforeExists := b.indexedPages[rule.before]
-	afterIndex, afterExists := b.indexedPages[rule.after]
-	if !beforeExists || !afterExists {
-		return true
-	}
-	return beforeIndex < afterIndex
 }
